7842557cfef04249e0170f6b45d77ad7cc02e2bb
divyanshu <divyanshu.r46956@gmail.com>
2016-12-23 20:17:20 +0530

    OPtimized code git diff!

--- routes/authenticate.js
+++ routes/authenticate.js
@@ -1,4 +1,15 @@
+
 var FacebookStrategy = require('passport-facebook').Strategy;
+
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var User = require('./models/user');
+var config = require('./config');
+
+exports.local = passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 
 exports.facebook = passport.use(new FacebookStrategy({
 
