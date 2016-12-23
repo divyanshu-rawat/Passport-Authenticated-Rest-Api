3fb7a781ecd83537457ee4577bfb402dd99344d4
divyanshu <divyanshu.r46956@gmail.com>
2016-12-23 21:58:38 +0530

    successfully imlemented fb oauth git add .!

--- routes/users.js
+++ routes/users.js
@@ -83,4 +83,36 @@ router.get('/logout', function(req, res) {
   });
 });
 
+router.get('/facebook', passport.authenticate('facebook'),
+  function(req, res){});
+
+router.get('/facebook/callback', function(req,res,next){
+  passport.authenticate('facebook', function(err, user, info) {
+    if (err) {
+      return next(err);
+    }
+    if (!user) {
+      return res.status(401).json({
+        err: info
+      });
+    }
+    req.logIn(user, function(err) {
+      if (err) {
+        return res.status(500).json({
+          err: 'Could not log in user'
+        });
+      }
+              var token = Verify.getToken(user);
+              res.status(200).json({
+        status: 'Login successful!',
+        success: true,
+        token: token
+      });
+    });
+  })(req,res,next);
+});
+
+
+
+
 module.exports = router;
