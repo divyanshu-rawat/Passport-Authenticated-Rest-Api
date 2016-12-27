8eb1f859e40960633520ac03f2f718a263858d4b
divyanshu <divyanshu.r46956@gmail.com>
2016-12-28 00:54:34 +0530

    implementing favorite route git add .!

--- models/favorites.js
+++ models/favorites.js
@@ -1,21 +1,17 @@
+
 var mongoose = require('mongoose');
 var Schema   = mongoose.Schema;
 
-var commentSchema = new Schema({
-    rating:  {
-        type: Number,
-        min: 1,
-        max: 5,
-        required: true
-    },
-    comment:  {
-        type: String,
-        required: true
-    },
+var favoriteScheme = new Schema({
     
     postedBy: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User'
+    },
+
+    favDish: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'dishes'
     }
 
 }, {
---
+++ routes/favoriteRoute.js
@@ -0,0 +1,126 @@
+
+var
+  express = require('express'),
+  bodyParser = require('body-parser');
+
+var mongoose = require('mongoose');
+var leaderships = require('../models/leadership');
+
+var Verify = require('../routes/verify');
+
+var leaderRouter = express.Router();
+
+leaderRouter.use(bodyParser.json());
+
+leaderRouter.route('/')
+
+  // .all(function(req,res,next) {
+  //   res.writeHead(200, { 'Content-Type': 'text/plain' });
+  //   next();
+  // })
+
+  .get(Verify.verifyOrdinaryUser,function(req,res,next){
+    // res.end('Will send all the leaders to you!');
+
+    leaderships.find({},function (err,promotion) {
+        
+          if(err) throw err;
+
+          res.json(promotion);
+        
+        });
+
+
+  })
+
+  .post(Verify.verifyOrdinaryUser,Verify.verifyAdmin,function(req, res, next){
+    // res.end('Will add the leader: ' + req.body.name + ' with details: ' + req.body.description);
+
+
+    leaderships.create(req.body,function (err,leadership) {
+      
+        if(err) throw err;
+        console.log('leadership Created !!');
+
+        var id = leadership._id;
+
+
+        res.writeHead(200, {
+            'Content-Type': 'text/plain'
+        });
+
+        res.end('Added the leadership with id: ' + id);
+
+    });
+
+  })
+
+  .delete(Verify.verifyOrdinaryUser,Verify.verifyAdmin,function(req, res, next){
+    // res.end('Deleting all leaders');
+
+     leaderships.remove({}, function (err, resp) {
+
+        if (err) throw err;
+        res.json(resp);
+    });
+
+
+  });
+
+  
+  leaderRouter.route('/:leaderId')
+  
+  // .all(function(req,res,next) {
+  //   res.writeHead(200, { 'Content-Type': 'text/plain' });
+  //   next();
+  // })
+
+  .get(Verify.verifyOrdinaryUser,function(req,res,next){
+    // res.end('Will send details of the leader: ' + req.params.leaderId +' to you!');
+
+
+       leaderships.findById(req.params.leaderId,function(err,leadership){
+
+          if(err) throw err;
+          res.json(leadership);
+
+        });
+
+  })
+
+  .put(Verify.verifyOrdinaryUser,Verify.verifyAdmin,function(req, res, next){
+
+    // res.write('Updating the leader: ' + req.params.leaderId + '\n');
+    // res.end('Will update the leader: ' + req.body.name +
+    //   ' with details: ' + req.body.description);
+
+
+     leaderships.findByIdAndUpdate(req.params.leaderId,{
+
+          $set:req.body
+
+        },
+        {
+          new:true
+        },function  (err,leadership) {
+          
+            if(err) throw err;
+            res.json(leadership);
+        });
+
+
+  })
+
+  .delete(Verify.verifyOrdinaryUser,Verify.verifyAdmin,function(req, res, next){
+    // res.end('Deleting leader: ' + req.params.leaderId);
+
+    leaderships.findByIdAndRemove(req.params.leaderId, function (err, resp) {  
+
+          if (err) throw err;
+        
+          res.json(resp);
+    });
+    
+  });
+
+module.exports = leaderRouter;
